fd3388fee2cab117c514d48ffd22a02b12c6f96c
Ankit Sarangi <sarangiankit097@gmail.com>
2024-05-23 08:47:17 +0530

    remove middleware

--- middleware.ts
+++ middleware.ts
@@ -1,24 +1,24 @@
-import { getToken } from "next-auth/jwt";
-import { NextRequest, NextResponse } from "next/server";
+// import { getToken } from "next-auth/jwt";
+// import { NextRequest, NextResponse } from "next/server";
 
-export async function middleware(req: NextRequest) {
-  const path = req.nextUrl.pathname;
-  const token = await getToken({
-    req: req,
-    secret: process.env.NEXTAUTH_SECRET,
-  });
-  console.log("Path:", path);
-  console.log("Token:", token);
-  const publicPaths = path === "/login" || path === "/register";
+// export async function middleware(req: NextRequest) {
+//   const path = req.nextUrl.pathname;
+//   const token = await getToken({
+//     req: req,
+//     secret: process.env.NEXTAUTH_SECRET,
+//   });
+//   console.log("Path:", path);
+//   console.log("Token:", token);
+//   const publicPaths = path === "/login" || path === "/register";
 
-  if (publicPaths && token) {
-    return NextResponse.redirect(new URL("/", req.nextUrl));
-  }
-  if (!publicPaths && !token) {
-    return NextResponse.redirect(new URL("/login", req.nextUrl));
-  }
-}
+//   if (publicPaths && token) {
+//     return NextResponse.redirect(new URL("/", req.nextUrl));
+//   }
+//   if (!publicPaths && !token) {
+//     return NextResponse.redirect(new URL("/login", req.nextUrl));
+//   }
+// }
 
-export const config = {
-  matcher: ["/", "/register", "/login"],
-};
+// export const config = {
+//   matcher: ["/", "/register", "/login"],
+// };
